8b291ebd12af9e86c1f0e7eb3b7da8fcec82ee6f
Sandeep <sandeepraj.singari@gmail.com>
2020-03-03 15:45:14 +0530

    Changes

---
+++ Assets/Scripts/Interactable/CrateManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CrateManager : MonoBehaviour
+{
+    [SerializeField] LayerMask playerLayer;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject != null)
+            Debug.Log("Collision working");
+
+        if (collision.gameObject.layer == playerLayer)
+            Debug.Log("Player and Layer Detected");
+    }
+}
