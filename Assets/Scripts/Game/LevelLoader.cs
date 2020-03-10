dcd31db0c45f3f747dd5e4ac2349238c43268bb6
Sandeep <sandeepraj.singari@gmail.com>
2020-03-10 11:11:14 +0530

    Adding UI and Level Managment

---
+++ Assets/Scripts/Game/LevelLoader.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+}
---
+++ Assets/Scripts/UI/Menus/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelManagement
+{
+    public class MainMenu : Menu<MainMenu>
+    {
+        public void OnPlayPressed ()
+        {
+            //LevelSelectionMenu
+        }
+
+        public void OnSettingPressed ()
+        {
+
+        }
+
+        public void OnCreditsPressed ()
+        {
+
+        }
+
+        public void OnQuitPressed ()
+        {
+            Application.Quit();
+        }
+    }
+} 
